c2bb2d2f2623a0fc0b133d50d89a14f3601afe24
Pierre MINIGGIO <=>
2020-02-03 10:08:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Simple Content Revealer
+
+Installation :
+```
+npm install pierreminiggio/content-revealer
+```
+
+Utilisation : 
+```javascript
+const ContentRevealer = require('@pierreminiggio/content-revealer')
+```
---
+++ main.js
@@ -0,0 +1,39 @@
+let addInstancesProto = require('@pierreminiggio/add-instance-proto')
+
+let ContentRevealerSingleton = (function () {
+    let buildInstance = function () {
+
+        this.load = function (querySelector = '.content-revealer') {
+            let singleton = this
+
+            document.querySelectorAll(querySelector).forEach(elt => {
+                if (elt.dataset.loaded === undefined) {
+                    elt.dataset.loaded = 1
+                    let target = elt.dataset.contentselector
+                    let group = elt.dataset.contentgroup !== undefined ? elt.dataset.contentgroup : 1
+
+                    // On repère un objet via son id, s'il n'en a pas on lui donne un prefix et un numéro
+                    //let identifier = singleton.createInstanceIdentifier(elt)
+
+                    singleton.loadedInstances[identifier] = elt
+                }
+            })
+        }
+    }
+
+    let instance = null
+    return new function () {
+        this.getInstance = function () {
+            if (instance == null) {
+                instance = new buildInstance()
+                instance.buildInstance = null
+            }
+            return instance
+        }
+    }
+})()
+
+let ContentRevealer = ContentRevealerSingleton.getInstance()
+addInstancesProto(ContentRevealer)
+
+module.exports = ContentRevealer
